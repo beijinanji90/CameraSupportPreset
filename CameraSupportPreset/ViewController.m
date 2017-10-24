09c16bcbe31cb92052babd5169e358bac80bc904
chenfenglong <chenfenglong@kuaishou.com>
2017-10-24 19:40:38 +0800

    camera支持的拍摄分辨率

--- CameraSupportPreset/ViewController.m
+++ CameraSupportPreset/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 
@@ -19,10 +20,53 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesBegan:touches withEvent:event];
+    [self printCamerasInfo];
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (CMVideoDimensions) getCameraMaxStillImageResolution:(AVCaptureDevicePosition) cameraPosition {
+    
+    CMVideoDimensions max_resolution;
+    max_resolution.width = 0;
+    max_resolution.height = 0;
+    
+    AVCaptureDevice *captureDevice = nil;
+    
+    NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for (AVCaptureDevice *device in devices) {
+        if ([device position] == cameraPosition) {
+            captureDevice = device;
+            break;
+        }
+    }
+    
+    if (captureDevice == nil) {
+        return max_resolution;
+    }
+    
+    NSArray* availFormats=captureDevice.formats;
+    
+    for (AVCaptureDeviceFormat* format in availFormats) {
+        CMVideoDimensions resolution = format.highResolutionStillImageDimensions;
+        int w = resolution.width;
+        int h = resolution.height;
+        if ((w * h) > (max_resolution.width * max_resolution.height)) {
+            max_resolution.width = w;
+            max_resolution.height = h;
+        }
+    }
+    
+    return max_resolution;
+}
+
+- (void) printCamerasInfo {
+    CMVideoDimensions res;
+    res = [self getCameraMaxStillImageResolution:AVCaptureDevicePositionBack];
+    NSLog(@" Back  Camera max Image resolution: %d x %d", res.width, res.height);
+    res = [self getCameraMaxStillImageResolution:AVCaptureDevicePositionFront];
+    NSLog(@" Front Camera max Image resolution: %d x %d", res.width, res.height);
 }
 
 
